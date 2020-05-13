b28c41aa7668d0172a04c6b45dbe3cd1db5d8cd6
rakeshAlgo <rakesh.algoworks.com>
2020-05-13 15:16:42 +0530

    removing ES lint error for is this page helpful task

--- src/js/06-page-rating.js
+++ src/js/06-page-rating.js
@@ -1,16 +1,16 @@
-;(function () {
+;
+(function () {
   'use strict'
-
   var dialogBox = document.getElementById('dialogBox')
   var helpYesBtn = document.getElementById('yesBtn')
   var helpNoBtn = document.getElementById('noBtn')
   var skipBtnMsg = document.getElementById('skipBtnMsg')
   var feedBackFormBox = document.getElementById('additionalFeedbackBox')
-   var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
-  var skipLeaveBtn = document.getElementById('skipLeaveBtn')
+  //  var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
+  // var skipLeaveBtn = document.getElementById('skipLeaveBtn')
   var feedBackMsg = document.querySelector('.feed-back-msg')
   var submitBtn = document.querySelector('.submit-btn')
-  var leaveYesBtn = document.querySelector('.yes-btn')
+  // var leaveYesBtn = document.querySelector('.yes-btn')
   var feedbackInfoBtn = document.querySelector('.info-btn')
   var feedbackModal = document.querySelector('.feedback-modal')
   var closeModalPopup = document.querySelector('.close-popup')
@@ -20,16 +20,16 @@
   var noBtnData = helpNoBtn.dataset
 
   helpYesBtn.addEventListener('click', function (e) {
-   // dialogBox.style.display = 'block'
+    // dialogBox.style.display = 'block'
     this.classList.add('active')
     helpNoBtn.classList.remove('active')
-   // console.log(yesBtnData, 16)
+    console.log(yesBtnData, 16)
   })
   helpNoBtn.addEventListener('click', function (e) {
-   // dialogBox.style.display = 'block'
+    // dialogBox.style.display = 'block'
     this.classList.add('active')
     helpYesBtn.classList.remove('active')
-   // console.log(noBtnData, 27)
+    console.log(noBtnData, 27)
   })
 
   anyFeedbackBtn.addEventListener('click', function (e) {
@@ -40,7 +40,6 @@
   })
 
   skipBtnMsg.addEventListener('click', function (e) {
-
     dialogBox.style.display = 'none'
     feedBackMsg.value = ''
   })
@@ -58,10 +57,6 @@
       submitBtn.classList.add('disabled')
     }
   })
-
-  // leaveYesBtn.addEventListener('click', function (e) {
-  //   leaveAddtinalBox.style.display = 'none'
-  // }) 
   feedbackInfoBtn.addEventListener('click', function (e) {
     feedbackModal.classList.add('show')
   })
