5473d83027c33285aab5142d180573c4683edfaf
rakeshAlgo <rakesh.algoworks.com>
2020-05-13 10:55:13 +0530

    is this helpful task completed

--- src/js/06-page-rating.js
+++ src/js/06-page-rating.js
@@ -6,7 +6,7 @@
   var helpNoBtn = document.getElementById('noBtn')
   var skipBtnMsg = document.getElementById('skipBtnMsg')
   var feedBackFormBox = document.getElementById('additionalFeedbackBox')
-  var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
+   var leaveAddtinalBox = document.getElementById('leaveAddtinalBox')
   var skipLeaveBtn = document.getElementById('skipLeaveBtn')
   var feedBackMsg = document.querySelector('.feed-back-msg')
   var submitBtn = document.querySelector('.submit-btn')
@@ -14,31 +14,40 @@
   var feedbackInfoBtn = document.querySelector('.info-btn')
   var feedbackModal = document.querySelector('.feedback-modal')
   var closeModalPopup = document.querySelector('.close-popup')
+  var anyFeedbackBtn = document.querySelector('.any-feedback-btn')
   // for config
   var yesBtnData = helpYesBtn.dataset
   var noBtnData = helpNoBtn.dataset
 
   helpYesBtn.addEventListener('click', function (e) {
-    dialogBox.style.display = 'block'
+   // dialogBox.style.display = 'block'
     this.classList.add('active')
     helpNoBtn.classList.remove('active')
-    console.log(yesBtnData, 16)
+   // console.log(yesBtnData, 16)
   })
   helpNoBtn.addEventListener('click', function (e) {
-    dialogBox.style.display = 'block'
+   // dialogBox.style.display = 'block'
     this.classList.add('active')
     helpYesBtn.classList.remove('active')
-    console.log(noBtnData, 27)
+   // console.log(noBtnData, 27)
+  })
+
+  anyFeedbackBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    dialogBox.style.display = 'block'
+    feedBackFormBox.style.display = 'block'
+    this.classList.add('active')
   })
 
   skipBtnMsg.addEventListener('click', function (e) {
-    feedBackFormBox.style.display = 'none'
-    leaveAddtinalBox.style.display = 'block'
+
+    dialogBox.style.display = 'none'
+    feedBackMsg.value = ''
   })
-  skipLeaveBtn.addEventListener('click', function (e) {
-    leaveAddtinalBox.style.display = 'none'
-    feedBackFormBox.style.display = 'block'
-  })
+  // skipLeaveBtn.addEventListener('click', function (e) {
+  //   leaveAddtinalBox.style.display = 'none'
+  //   feedBackFormBox.style.display = 'block'
+  // })
 
   feedBackMsg.addEventListener('keyup', function (e) {
     var textareaValue = this.value
@@ -49,9 +58,10 @@
       submitBtn.classList.add('disabled')
     }
   })
-  leaveYesBtn.addEventListener('click', function (e) {
-    leaveAddtinalBox.style.display = 'none'
-  })
+
+  // leaveYesBtn.addEventListener('click', function (e) {
+  //   leaveAddtinalBox.style.display = 'none'
+  // }) 
   feedbackInfoBtn.addEventListener('click', function (e) {
     feedbackModal.classList.add('show')
   })
