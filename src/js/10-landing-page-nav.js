2fc0dd94a35af07682c5e2b4aca0e4a4a96b2793
rakeshAlgo <rakesh.algoworks.com>
2020-05-21 13:42:16 +0530

    filter functionality done for multiple select

--- src/js/10-landing-page-nav.js
+++ src/js/10-landing-page-nav.js
@@ -1,31 +1,80 @@
-;(function () {
-    'use strict'
+(function($) {
 
-    var clearAllBtn = document.querySelector('#clearALLBtn')
-    var menuList = document.querySelectorAll('.nav-list .nav-link')
+  'use strict';
 
-    clearAllBtn.addEventListener('click', function (e) {
-        e.preventDefault()
-        var inputs = document.querySelectorAll('.check-mark')
-        for(var i = 0; i < inputs.length; i++) {
-          inputs[i].checked = false
+  // for slide toggle
+  $( ".nav-link" ).click(function(){
+    $(this).toggleClass('active')
+    $(this).next('.sub-menu').slideToggle(10)
+  })
+
+
+  // for filter menu
+  var $filterCheckboxes = $('input[type="checkbox"]');
+
+  $filterCheckboxes.on('change', function() {
+
+  var selectedFilters = {
+
+  };
+
+   $filterCheckboxes.filter(':checked').each(function() {
+
+    if (!selectedFilters.hasOwnProperty(this.name)) {
+      selectedFilters[this.name] = [];
+      //console.log(selectedFilters, 23)
+    }
+
+    selectedFilters[this.name].push(this.value);
+    console.log(selectedFilters, 27)
+  });
+
+  // create a collection containing all of the filterable elements
+  var $filteredResults = $('.data-filter-column');
+
+  // loop over the selected filter name -> (array) values pairs
+  $.each(selectedFilters, function(name, filterValues) {
+
+    // filter each .data-filter-column element
+    $filteredResults = $filteredResults.filter(function() {
+
+      var matched = false,
+        currentFilterValues = $(this).find('.sub-heading').data('level').split(' ');
+       var test = $(this).find('.languages li p').text();
+        console.log(currentFilterValues, test, 48);
+
+      // loop over each category value in the current .data-filter-column's data-category
+      $.each(currentFilterValues, function(_, currentFilterValue) {
+        console.log('enter');
+
+        // if the current category exists in the selected filters array
+        // set matched to true, and stop looping. as we're ORing in each
+        // set of filters, we only need to match once
+
+        if ($.inArray(currentFilterValue, filterValues) != -1) {
+          console.log('true');
+          matched = true;
+          return false;
         }
-      })
+      });
 
-        // function menuClickEvent(id) {
-        //     for(var i = 0; i < menuList.length; i++) {
-        //             console.log(menuList[i], id, menuList[id])
-        //       }
+      // if matched is true the current .data-filter-column element is returned
+      return matched;
 
-        // }
-        // Handlebars.registerHelper("menuClickEvent", function(id) {
-        //     console.log("hello", id)
-        //   });
+    });
+  });
 
-     //   document.getElementsByClassName('nav-link').addEventListener('click', menuClickEvent)
-        //   menuClick.addEventListener('click', function(e){
+  $('.data-filter-column').addClass('hide').filter($filteredResults).addClass('show').removeClass('hide');
+    // reset all check mark
+    $('#clearALLBtn').click(function(event){
+      selectedFilters = [];
+      $('.data-filter-column').removeClass('hide')
+      var inputs = $('.check-mark')
+      for(var i = 0; i < inputs.length; i++) {
+        inputs[i].checked = false
+      }
+    })
+});
 
-    //   })
 
-
-  })()
+})(jQuery);
