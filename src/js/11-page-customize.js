9e12f0bb51aa0523dbd600ddda0115c56f1848d7
rakeshAlgo <rakesh.algoworks.com>
2020-06-25 00:35:22 +0530

    UI issues fixes from doc sheet

--- src/js/11-page-customize.js
+++ src/js/11-page-customize.js
@@ -1,14 +1,26 @@
 ;(function ($) {
   'use strict'
+  var tableBlock = document.querySelectorAll('table.tableblock')
   // for label edition/statuses
   var $labels = $('.edition').find('a')
-  // console.log($labels.length)
+
   for (var i = 0; i < $labels.length; i++) {
   //  console.log(i, 445, $labels[i])
     if ($labels[i].text.toLocaleLowerCase().indexOf('community') !== -1) {
       $labels[i].parentNode.classList.add('page-edition')
     }
   }
+
+  // add a caption class for all tablelock
+
+  tableBlock.forEach(function(arr, index) {
+    console.log(arr)
+    // if(arr.caption.title)
+    if(arr.caption !== null) {
+      arr.classList.add('caption-table')
+    }
+
+  })
 
   /*eslint-env jquery*/
 })(jQuery)
