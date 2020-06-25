a827f5dc3221da411b3fe0319c4231d40a9979ea
rakeshAlgo <rakesh.algoworks.com>
2020-06-25 08:46:58 +0530

    Remove border under  heading nd fix Eslint error

--- src/js/11-page-customize.js
+++ src/js/11-page-customize.js
@@ -3,24 +3,16 @@
   var tableBlock = document.querySelectorAll('table.tableblock')
   // for label edition/statuses
   var $labels = $('.edition').find('a')
-
   for (var i = 0; i < $labels.length; i++) {
-  //  console.log(i, 445, $labels[i])
     if ($labels[i].text.toLocaleLowerCase().indexOf('community') !== -1) {
       $labels[i].parentNode.classList.add('page-edition')
     }
   }
-
   // add a caption class for all tablelock
-
-  tableBlock.forEach(function(arr, index) {
-    console.log(arr)
-    // if(arr.caption.title)
-    if(arr.caption !== null) {
-      arr.classList.add('caption-table')
+  tableBlock.forEach(function (elem, index) {
+    if (elem.caption !== null) {
+      elem.classList.add('caption-table')
     }
-
   })
-
   /*eslint-env jquery*/
 })(jQuery)
