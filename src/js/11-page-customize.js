3c46817e221296bc36b94ff2e5682524ff4c78b9
Dan Allen <dan@opendevise.com>
2020-06-26 03:47:52 -0600

    load required icons
    - allow icons to be specified explicitly as required in src/js/vendor/fontawesome-icon-defs.js
    - use copy icon from Font Awesome in source block toolbar
    - use GitHub icon from Font Awesome in source block toolbar
    - fix styles for source block toolbar
    - add Font Awesome placeholder for section title anchors
    - use link icon from Font Awesome for section title anchors
    - fix styles for section title anchors

--- src/js/11-page-customize.js
+++ src/js/11-page-customize.js
@@ -1,10 +1,14 @@
 ;(function () {
   'use strict'
-
   // hide admonition icons from Font Awesome i2svg
   // FIXME: eventually we want to add the right prefix so that these icons get loaded from Font Awesome
   ;[].slice.call(document.querySelectorAll('td.icon > i.fa')).forEach(function (el) {
     el.classList.remove('fa')
+  })
+  // add fa placeholder for heading anchors
+  ;[].slice.call(document.querySelectorAll('a.anchor')).forEach(function (a) {
+    if (!/^H[1-6]$/.test(a.parentNode.tagName)) return
+    a.appendChild(document.createElement('i')).className = 'fas fa-link'
   })
   // for label edition/statuses
   ;[].slice.call(document.querySelectorAll('.edition a')).forEach(function (a) {
