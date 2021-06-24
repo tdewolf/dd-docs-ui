23b8e5fa4be8c1149b8d0f8a3384cd49afd79288
anmol-algo <anmol.tripathi@algoworks.com>
2021-06-24 17:55:54 +0530

    update the code source block

--- src/js/07-copy-to-clipboard.js
+++ src/js/07-copy-to-clipboard.js
@@ -1,6 +1,7 @@
 ;(function () {
   'use strict'
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+    console.log(codeBlock,4)
     var viewSourceLink
     var sourceUrl = codeBlock.dataset.sourceUrl
     if (sourceUrl) {
@@ -13,10 +14,19 @@
     }
     var sourceTypeBox = document.createElement('div')
     sourceTypeBox.className = 'source-type-box'
+
+    var headingBox = document.createElement('div')
+    headingBox.className = 'col-2 left-block'
+
+    var sourceTypeBoxCol2 = document.createElement('div')
+    sourceTypeBoxCol2.className = 'col-2 right-block'
+
     var copyButton = document.createElement('a')
     copyButton.className = 'copy-code-button'
     copyButton.dataset.title = 'Copy'
     copyButton.appendChild(document.createElement('i')).className = 'far fa-copy'
+    var copyText = document.createTextNode('Copy')
+    copyButton.appendChild(copyText)
 
     var dataSource = document.createElement('span')
     dataSource.className = 'data-source'
@@ -24,6 +34,15 @@
 
     var fadeShadow = document.createElement('span')
     fadeShadow.className = 'fade-shadow'
+
+    var runCode = document.createElement('a')
+    runCode.className= 'run-code'
+    runCode.dataset.title = 'Run Code'
+    runCode.href= '#'
+    runCode.target = '_blank'
+    runCode.appendChild(document.createElement('i')).className = 'far fa-code'
+    var runCodeText = document.createTextNode('Run Code')
+    runCode.appendChild(runCodeText)
 
     copyButton.addEventListener('click', function (e) {
       // NOTE: ignore event on pseudo-element
@@ -66,10 +85,13 @@
       }
     })
     var pre = codeBlock.parentNode
-    pre.appendChild(sourceTypeBox)
-    sourceTypeBox.appendChild(dataSource)
-    if (viewSourceLink) sourceTypeBox.appendChild(viewSourceLink)
-    sourceTypeBox.appendChild(copyButton)
+    pre.prepend(sourceTypeBox)
+    sourceTypeBox.appendChild(headingBox)
+    sourceTypeBox.appendChild(sourceTypeBoxCol2)
+    sourceTypeBoxCol2.appendChild(dataSource)
+    if (viewSourceLink) sourceTypeBoxCol2.appendChild(viewSourceLink)
+    sourceTypeBoxCol2.appendChild(copyButton)
+    sourceTypeBoxCol2.appendChild(runCode)
     pre.appendChild(fadeShadow)
   })
 })()
