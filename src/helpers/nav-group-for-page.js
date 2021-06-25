2622d03e8faa1b13012be790c7cc3c8c0f7a4fad
Dan Allen <dan@opendevise.com>
2021-06-25 00:33:44 -0600

    fix navigation for contribution guide in home component

--- src/helpers/nav-group-for-page.js
+++ src/helpers/nav-group-for-page.js
@@ -1,7 +1,9 @@
 'use strict'
 
 module.exports = (navGroups, { data: { root: { page, site } } }) => {
+  const pageUrl = page.url
+  const navGroupByUrl = navGroups.find(({ url }) => url === pageUrl)
+  if (navGroupByUrl) return navGroupByUrl
   const pageComponentName = page.component.name
-  if (pageComponentName === 'home') return navGroups.find(({ url }) => url === page.url)
   return navGroups.find(({ components }) => ~components.indexOf(pageComponentName))
 }
