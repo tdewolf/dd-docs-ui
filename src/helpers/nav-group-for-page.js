8e68bae1dc09f02b176644e4cc52175602875b01
Dan Allen <dan@opendevise.com>
2021-07-16 16:24:11 -0600

    only show Home nav group for contribute module

--- src/helpers/nav-group-for-page.js
+++ src/helpers/nav-group-for-page.js
@@ -5,5 +5,6 @@ module.exports = (navGroups, { data: { root: { page, site } } }) => {
   const navGroupByUrl = navGroups.find(({ url }) => url === pageUrl)
   if (navGroupByUrl) return navGroupByUrl
   const pageComponentName = page.component.name
+  if (pageComponentName === 'home' && page.module !== 'contribute') return
   return navGroups.find(({ components }) => ~components.indexOf(pageComponentName))
 }
