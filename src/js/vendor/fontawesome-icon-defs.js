b10144009bd609a8632bc7fa1444666dea305942
Ray Offiah <ray.offiah@couchbase.com>
2021-12-01 17:10:16 +0000

    Changing to the regular font to make the link icons a bit easier on the eye.

--- src/js/vendor/fontawesome-icon-defs.js
+++ src/js/vendor/fontawesome-icon-defs.js
@@ -1,2 +1,2 @@
-/*! iconNames: ['far fa-copy', 'fas fa-link', 'fab fa-github', 'fas fa-terminal', 'fas fa-external-link-alt'] */
+/*! iconNames: ['far fa-copy', 'fas fa-link', 'fab fa-github', 'fas fa-terminal', 'far fa-external-link-alt'] */
 window.FontAwesomeIconDefs = []
